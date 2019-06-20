ddd362e4c056e562ea6f74dde6f67eef86abb292
Amir Talley <ta422@zoo.cs.yale.edu>
2019-06-20 18:09:51 -0400

    Added functions to verify in oracle

--- oracle/verify.go
+++ oracle/verify.go
@@ -2,45 +2,100 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"strconv"
+	"strings"
+	"time"
 )
 
 // Function type that returns a function to test whether data meets user-specified criteria
 type UserDefinedIntegrity func(int) bool
 
+// Variance allowed for data
+var sigma float64 = 1.5
+
 // Examples of user-defined integrity statements
-func valid_year(year int) bool {
-	if year > 2019 {
+func valid_record_year(year int) bool {
+	// For a record to be valid, it must be measured before the current time
+	dt := time.Now()
+	current_year, _ := strconv.Atoi(strings.Split(dt.Format("01-02-2006"), "-")[2])
+
+	// Check that the record year is smaller than the current year
+	if year > current_year {
 		return false
 	} else {
 		return true
 	}
 }
 
-// Given a set of numbers, score the quality of the data
-func data_score(dat []int, params ...UserDefinedIntegrity) int {
-	score := 0
-	for _, param := range params {
-		for _, num := range dat {
+// Given a set of numbers, score the quality of the data from 0 to 5
+func data_score(dat []int, params ...UserDefinedIntegrity) float64 {
+	avg := average(dat)
+	stdev := stdev(dat)
+	n := float64(len(dat))
+	p := float64(len(params))
+	score := 0.0
+	for _, num := range dat {
+		for _, param := range params {
 			if param(num) {
-				score += 1
-			} else {
-				score -= 1
+				score += (1.0 / (n * p))
 			}
 		}
+
+		if math.Abs(float64(num)-avg) < sigma*stdev {
+			score += (1.0 / n)
+		}
 	}
-	return score
+
+	return score * 5.0 / (p + 1.0)
 }
 
 // Given a set of data, determine the most accurate data value
 func data_value(dat []int) int {
-	return 0
+	// Take average of values within 1 standard deviation of the mean
+	avg := average(dat)
+	stdev := stdev(dat)
+	val := 0.0
+	ctr := 0.0
+	for _, num := range dat {
+		if math.Abs(float64(num)-avg) < sigma*stdev {
+			val += float64(num)
+			ctr += 1.0
+		}
+	}
+	val = val / ctr
+
+	return int(val)
+}
+
+// Given a set of data, determine the precision of the data
+func stdev(dat []int) float64 {
+	avg := average(dat)
+	res := 0.0
+	for _, num := range dat {
+		res += math.Pow(avg-float64(num), 2)
+	}
+	res = res / float64(len(dat))
+	res = math.Sqrt(res)
+
+	return res
+}
+
+// Computes the average of a set of data
+func average(dat []int) float64 {
+	avg := 0.0
+	for _, num := range dat {
+		avg += float64(num)
+	}
+	avg = avg / float64(len(dat))
+
+	return avg
 }
 
 func main() {
-	// Test cases
-	dat := []int{2011, 2010, 2019, 2020, 2040, 78}
-	fmt.Println(data_score(dat, valid_year))
-	fmt.Println(data_value(dat))
-	fmt.Println(valid_year(2017))
-	fmt.Println(valid_year(2020))
+	// Example use case: dat holds measurements of temperature taken from IoT devices
+	// Devices are in the same general region and measurements are taken within the same hour
+	dat := []int{0, 70, 71, 72, 71, 75, 89, 71}
+	fmt.Println(data_value(dat)) // 74, which is approximately the average temperature from these
+	fmt.Println(data_score(dat)) // 4.4 out of 5, which makes sense because only 2 values are "off"
 }
