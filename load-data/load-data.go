491d02ba0e878b6384035b3d24d4b52795fa39ff
Varunram <vrg2009@ymail.com>
2019-06-20 17:17:58 -0400

    beautify load-data

--- load-data/load-data.go
+++ load-data/load-data.go
@@ -1,32 +1,33 @@
 package main
 
 import (
+	"bytes"
 	"encoding/csv"
-	"fmt"
 	"io/ioutil"
 	"log"
-	"os"
-	"strings"
 )
 
-func create_csv_dictionary(path_to_file string) [][]string {
-	file, err := os.Open(path_to_file)
+func createCsvDictionary(filePath string) ([][]string, error) {
+	var records [][]string
+	byteData, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	in, err := ioutil.ReadAll(file)
-	r := csv.NewReader(strings.NewReader(string(in)))
-	records, err := r.ReadAll()
-	if err != nil {
-		log.Fatal(err)
+		return records, err
 	}
 
-	return records
+	csvReader := csv.NewReader(bytes.NewReader(byteData))
+	records, err = csvReader.ReadAll()
+	if err != nil {
+		return records, err
+	}
+
+	return records, nil
 }
 
 func main() {
-	// Example call to create_csv_dictionary
-	fmt.Println(create_csv_dictionary("data/country_definitions.csv"))
+	// Example call to createCsvDictionary
+	records, err := createCsvDictionary("data/country_definitions.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(records)
 }
