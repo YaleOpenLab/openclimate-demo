8423e955fe830ff750d198099d798174f39da3d5
Varunram <vrg2009@ymail.com>
2019-06-27 16:53:20 -0400

    Add ocean data in

--- server/data.go
+++ server/data.go
@@ -3,6 +3,7 @@ package server
 import (
 	"encoding/json"
 	"github.com/YaleOpenLab/openclimate/database"
+	utils "github.com/YaleOpenLab/openx/utils"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -12,6 +13,7 @@ func dataHandler() {
 	getUSStates()
 	getUSCounties()
 	getParisAgreement()
+	getOceanData()
 }
 
 type USStatesReturn struct {
@@ -122,6 +124,79 @@ func getParisAgreement() {
 			temp.Percentage = values.Percentage
 			temp.Year = values.Year
 			y[values.Code] = temp
+		}
+		MarshalSend(w, y)
+	})
+}
+
+type OceanDataPrelim struct {
+	Year              int     `json:"year"`
+	OceanSink         float64 `json:"Ocean-Sink"`
+	CCSMBEC           float64 `json:"CCSM-BEC"`
+	MITgcmREcoM2      float64 `json:"MITgcm-REcoM2"`
+	MPIOMHAMOCC       float64 `json:"MPIOM-HAMOCC"`
+	NEMO36PISCESv2gas float64 `json:"NEMO3.6-PISCESv2-gas (CNRM)""`
+	NEMOPISCESIPSL    float64 `json:"NEMO-PISCES (IPSL)"`
+	NEMOPlankTOM5     float64 `json:"NEMO-PlankTOM5"`
+	NorESMOC          float64 `json:"NorESM-OC"`
+	Landschutzer      float64 `json:"Landschützer"`
+	Rodenbeck         float64 `json:"Rödenbeck"`
+}
+
+type OceanDataFinal struct {
+	OceanSink         float64 `json:"Ocean-Sink"`
+	CCSMBEC           float64 `json:"CCSM-BEC"`
+	MITgcmREcoM2      float64 `json:"MITgcm-REcoM2"`
+	MPIOMHAMOCC       float64 `json:"MPIOM-HAMOCC"`
+	NEMO36PISCESv2gas float64 `json:"NEMO3.6-PISCESv2-gas (CNRM)""`
+	NEMOPISCESIPSL    float64 `json:"NEMO-PISCES (IPSL)"`
+	NEMOPlankTOM5     float64 `json:"NEMO-PlankTOM5"`
+	NorESMOC          float64 `json:"NorESM-OC"`
+	Landschutzer      float64 `json:"Landschützer"`
+	Rodenbeck         float64 `json:"Rödenbeck"`
+}
+
+func getOceanData() {
+	http.HandleFunc("/ocean/data", func(w http.ResponseWriter, r *http.Request) {
+		checkGet(w, r)
+		checkOrigin(w, r)
+
+		_, err := authorizeUser(r)
+		if err != nil {
+			log.Println("could not retrieve user from the database, quitting")
+			responseHandler(w, StatusBadRequest)
+			return
+		}
+
+		data, err := ioutil.ReadFile("load-data/json_data/ocean_sink.json")
+		if err != nil {
+			log.Println(err)
+			responseHandler(w, StatusInternalServerError)
+			return
+		}
+
+		var x map[string]OceanDataPrelim
+		err = json.Unmarshal(data, &x)
+		if err != nil {
+			log.Println(err)
+			responseHandler(w, StatusInternalServerError)
+			return
+		}
+
+		y := make(map[string]OceanDataFinal)
+		for _, values := range x {
+			var temp OceanDataFinal
+			temp.OceanSink = values.OceanSink
+			temp.CCSMBEC = values.CCSMBEC
+			temp.MITgcmREcoM2 = values.MITgcmREcoM2
+			temp.MPIOMHAMOCC = values.MPIOMHAMOCC
+			temp.NEMO36PISCESv2gas = values.NEMO36PISCESv2gas
+			temp.NEMOPISCESIPSL = values.NEMOPISCESIPSL
+			temp.NEMOPlankTOM5 = values.NEMOPlankTOM5
+			temp.NorESMOC = values.NorESMOC
+			temp.Landschutzer = values.Landschutzer
+			temp.Rodenbeck = values.Rodenbeck
+			y[utils.ItoS(values.Year)] = temp
 		}
 		MarshalSend(w, y)
 	})
