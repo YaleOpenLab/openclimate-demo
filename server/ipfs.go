1b085096c3e4afc8ee5ca650ba003f7340d023d3
Brian Li <brianli1117@gmail.com>
2019-07-15 15:02:57 -0400

    ipfs file

--- server/ipfs.go
+++ server/ipfs.go
@@ -1,2 +1,16 @@
 package server
 
+import (
+
+	ipfs "github.com/Varunram/essentials/ipfs"
+
+)
+
+func ReportAndCommitData() {
+	http.HandleFunc("/user/report", func(w http.ResponseWriter, r *http.Request){
+		err := erpc.CheckGet(w, r)
+		if err != nil {
+			return
+		}
+	})
+}
