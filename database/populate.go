432ebb59a656095aa7bf81902737a6b087068ac1
Brian Li <brianli1117@gmail.com>
2019-07-17 15:26:07 -0400

    added populate users, regions

--- database/populate.go
+++ database/populate.go
@@ -7,6 +7,8 @@ import (
 func Populate() {
 	PopulateUSStates()
 	PopulateCountries()
+	PopulateRegionsTest()
+	PopulateAdminUsers()
 }
 
 // Test function populating the regions bucket with the US states
@@ -23,11 +25,41 @@ func PopulateUSStates() {
 
 func PopulateRegionsTest() {
 	_, err := NewRegion("Shanghai", "China")
+	if err != nil {
+		log.Println(err, "Failed populate regions test")
+		return
+	}
 	_, err = NewRegion("Osaka", "Japan")
+	if err != nil {
+		log.Println(err, "Failed populate regions test")
+		return
+	}
 	_, err = NewRegion("Cancun", "Mexico")
+	if err != nil {
+		log.Println(err, "Failed populate regions test")
+		return
+	}
 	_, err = NewRegion("Addis Ababa", "Ethiopia")
-	log.Println(err)
+	if err != nil {
+		log.Println(err, "Failed populate regions test")
+		return
+	}
 }
+
+
+func PopulateAdminUsers() {
+	_, err := NewUser("amanda", "9a768ace36ff3d1771d5c145a544de3d68343b2e76093cb7b2a8ea89ac7f1a20c852e6fc1d71275b43abffefac381c5b906f55c3bcff4225353d02f1d3498758", "amanda@test.com", "individual")
+	if err != nil {
+		log.Println(err, "Failed populate users test")
+		return
+	}
+	_, err = NewUser("brian", "e9a75486736a550af4fea861e2378305c4a555a05094dee1dca2f68afea49cc3a50e8de6ea131ea521311f4d6fb054a146e8282f8e35ff2e6368c1a62e909716", "brian@test.com", "individual")
+	if err != nil {
+		log.Println(err, "Failed populate users test")
+		return
+	}
+}
+
 
 // Test function populating the countries bucket with dummy values
 // to test the rpc endpoint for countries
