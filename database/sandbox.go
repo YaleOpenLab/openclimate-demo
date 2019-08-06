6a1176fed72a15bd7140778a53b2d1a2e134ac95
brianli <brianli1117@gmail.com>
2019-08-06 11:11:41 -0400

    populate state bucket with US states

--- database/sandbox.go
+++ database/sandbox.go
@@ -19,7 +19,7 @@ func Populate() {
 func PopulateUSStates() error {
 	USStates = []string{"Alabama", "Alaska", "American Samoa", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Federated States of Micronesia", "Florida", "Georgia", "Guam", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Marshall Islands", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Northern Mariana Islands", "Ohio", "Oklahoma", "Oregon", "Palau", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virgin Island", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"}
 	for _, state := range USStates {
-		_, err := NewRegion(state, "USA")
+		_, err := NewState(state, "USA")
 		if err != nil {
 			return errors.Wrap(err, "could not populate US States")
 		}
