412447647384a9cd6ac78d104ef3d927d48bcd72
Varunram <vrg2009@ymail.com>
2019-08-19 14:07:46 +0530

    update pkg names and fn calls

--- sprojects/proofs/test.go
+++ sprojects/proofs/test.go
@@ -1,59 +1,21 @@
 package main
 
 import (
-	"crypto/rand"
-	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
 	"github.com/pkg/errors"
-	"io"
 	"log"
 	"math/big"
 
 	// btcutils "github.com/bithyve/research/utils"
 	utils "github.com/Varunram/essentials/utils"
 	bech32 "github.com/bithyve/research/bech32"
-	points "github.com/bithyve/research/points"
 	bitcoinrpc "github.com/bithyve/research/rpc"
+	btcutils "github.com/bithyve/research/utils"
 	"github.com/btcsuite/btcd/btcec"
 )
 
 var Curve *btcec.KoblitzCurve = btcec.S256() // take only the curve, can't use other stuff
-
-func Sha256(inputs ...[]byte) []byte {
-	shaNew := sha256.New()
-	for _, input := range inputs {
-		shaNew.Write(input)
-	}
-	return shaNew.Sum(nil)
-}
-
-func NewPrivateKey() (*big.Int, error) {
-	b := make([]byte, Curve.Params().BitSize/8+8)
-	_, err := io.ReadFull(rand.Reader, b)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var one = new(big.Int).SetInt64(1)
-	x := new(big.Int).SetBytes(b)
-	n := new(big.Int).Sub(Curve.Params().N, one)
-	x.Mod(x, n)
-	x.Add(x, one)
-
-	return x, nil
-}
-
-func PubkeyPointsFromPrivkey(privkey *big.Int) (*big.Int, *big.Int) {
-	x, y := Curve.ScalarBaseMult(privkey.Bytes())
-	return x, y
-}
-
-func PointFromPrivkey(privkey *big.Int) points.Point {
-	var x points.Point
-	x.X, x.Y = Curve.ScalarBaseMult(privkey.Bytes())
-	return x
-}
 
 func testAddHomomorphic(P1x, P1y, P2x, P2y, x1, x2 *big.Int) error {
 	Sumx, Sumy := Curve.Add(P1x, P1y, P2x, P2y)
@@ -69,26 +31,26 @@ func testAddHomomorphic(P1x, P1y, P2x, P2y, x1, x2 *big.Int) error {
 }
 
 func genCommitment() {
-	x1, err := NewPrivateKey() // lets assume this to be the same as x
+	x1, err := btcutils.NewPrivateKey() // lets assume this to be the same as x
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	x2, err := NewPrivateKey()
+	x2, err := btcutils.NewPrivateKey()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	P1x, P1y := PubkeyPointsFromPrivkey(x1)
-	P2x, P2y := PubkeyPointsFromPrivkey(x2)
+	P1x, P1y := btcutils.PubkeyPointsFromPrivkey(x1)
+	P2x, P2y := btcutils.PubkeyPointsFromPrivkey(x2)
 
 	err = testAddHomomorphic(P1x, P1y, P2x, P2y, x1, x2)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	shaBytes := Sha256(Curve.Params().Gx.Bytes(), Curve.Params().Gy.Bytes()) // SHA256(G)
-	Hx, Hy := Curve.ScalarBaseMult(shaBytes)                                 // points.Point(SHA256(G))
+	shaBytes := btcutils.Sha256(Curve.Params().Gx.Bytes(), Curve.Params().Gy.Bytes()) // btcutils.Sha256(G)
+	Hx, Hy := Curve.ScalarBaseMult(shaBytes)                                          // btcutils.Point(btcutils.Sha256(G))
 
 	var a []byte
 	a = []byte{1}
@@ -100,15 +62,15 @@ func genCommitment() {
 }
 
 func signCommitment() {
-	x, err := NewPrivateKey() // lets assume this to be the same as x
+	x, err := btcutils.NewPrivateKey() // lets assume this to be the same as x
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	Px, Py := PubkeyPointsFromPrivkey(x) // P = x*G
+	Px, Py := btcutils.PubkeyPointsFromPrivkey(x) // P = x*G
 
-	shaBytes := Sha256(Curve.Params().Gx.Bytes(), Curve.Params().Gy.Bytes()) // SHA256(G)
-	Hx, Hy := Curve.ScalarBaseMult(shaBytes)                                 // points.Point(SHA256(G))
+	shaBytes := btcutils.Sha256(Curve.Params().Gx.Bytes(), Curve.Params().Gy.Bytes()) // btcutils.Sha256(G)
+	Hx, Hy := Curve.ScalarBaseMult(shaBytes)                                          // btcutils.Point(btcutils.Sha256(G))
 
 	// var a []byte
 	// a = []byte{1}
@@ -120,7 +82,7 @@ func signCommitment() {
 
 	Cprx, Cpry := Curve.Add(Px, Py, new(big.Int).Neg(oneHx), new(big.Int).Neg(oneHy))
 
-	CprHash := Sha256(Cprx.Bytes(), Cpry.Bytes())
+	CprHash := btcutils.Sha256(Cprx.Bytes(), Cpry.Bytes())
 	CprHashHex := hex.EncodeToString(CprHash)
 
 	privkey, err := bech32.PrivKeyToWIF("testnet", true, x.Bytes())
@@ -150,28 +112,28 @@ func Create21AOSSig() (*big.Int, *big.Int, *big.Int, *big.Int, *big.Int, *big.In
 	// Then lets assume Dom wants to create a ring signature over C1 and C2 where
 	// C1 = xG + 1H and C2 = xG where x is the blinding factor
 
-	b, err := NewPrivateKey()
+	b, err := btcutils.NewPrivateKey()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	d, err := NewPrivateKey()
+	d, err := btcutils.NewPrivateKey()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	Pbx, Pby := PubkeyPointsFromPrivkey(b)
-	Pdx, Pdy := PubkeyPointsFromPrivkey(d)
+	Pbx, Pby := btcutils.PubkeyPointsFromPrivkey(b)
+	Pdx, Pdy := btcutils.PubkeyPointsFromPrivkey(d)
 
-	x, err := NewPrivateKey()
+	x, err := btcutils.NewPrivateKey()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	xGx, xGy := Curve.ScalarBaseMult(x.Bytes()) // xG
 
-	shaBytes := Sha256(Curve.Params().Gx.Bytes(), Curve.Params().Gy.Bytes()) // SHA256(G)
-	Hx, Hy := Curve.ScalarBaseMult(shaBytes)                                 // H = points.Point(SHA256(G))
+	shaBytes := btcutils.Sha256(Curve.Params().Gx.Bytes(), Curve.Params().Gy.Bytes()) // btcutils.Sha256(G)
+	Hx, Hy := Curve.ScalarBaseMult(shaBytes)                                          // H = btcutils.Point(btcutils.Sha256(G))
 
 	one := []byte{1}
 	oneHx, oneHy := Curve.ScalarMult(Hx, Hy, one) // 1*H
@@ -184,7 +146,7 @@ func Create21AOSSig() (*big.Int, *big.Int, *big.Int, *big.Int, *big.Int, *big.In
 	C2 := append(C2x.Bytes(), C2y.Bytes()...)
 	m = append(m, append(C1, C2...)...)
 
-	kd, err := NewPrivateKey() // random nonce for ring sig
+	kd, err := btcutils.NewPrivateKey() // random nonce for ring sig
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -193,9 +155,9 @@ func Create21AOSSig() (*big.Int, *big.Int, *big.Int, *big.Int, *big.Int, *big.In
 
 	BrianNodeNumber := []byte{2} // assume brian has node number 2
 
-	eb := Sha256(Kdx.Bytes(), Kdy.Bytes(), m, BrianNodeNumber)
+	eb := btcutils.Sha256(Kdx.Bytes(), Kdy.Bytes(), m, BrianNodeNumber)
 
-	sb, err := NewPrivateKey() // choose a signature sb at random fro brian
+	sb, err := btcutils.NewPrivateKey() // choose a signature sb at random fro brian
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -208,7 +170,7 @@ func Create21AOSSig() (*big.Int, *big.Int, *big.Int, *big.Int, *big.Int, *big.In
 
 	DomNodeNumber := []byte{1}
 
-	ed := Sha256(Kbx.Bytes(), Kby.Bytes(), m, DomNodeNumber) // ed = H(Kb || m || D)
+	ed := btcutils.Sha256(Kbx.Bytes(), Kby.Bytes(), m, DomNodeNumber) // ed = H(Kb || m || D)
 	// log.Println("ed: ", ed)
 
 	edd := new(big.Int).Mul(new(big.Int).SetBytes(ed), d) // ed * d
@@ -230,7 +192,7 @@ func Verify21AOSSig() {
 	minusedPby := new(big.Int).Neg(ebPby)
 	Kbx, Kby := Curve.Add(sbGx, sbGy, ebPbx, new(big.Int).Mod(minusedPby, Curve.P))
 
-	ed := Sha256(Kbx.Bytes(), Kby.Bytes(), m, DomNodeNumber)
+	ed := btcutils.Sha256(Kbx.Bytes(), Kby.Bytes(), m, DomNodeNumber)
 	// log.Println("ed: ", ed)
 
 	sdGx, sdGy := Curve.ScalarBaseMult(sd.Bytes())
@@ -239,7 +201,7 @@ func Verify21AOSSig() {
 	minusedPdy := new(big.Int).Neg(edPdy)
 	Kdx, Kdy := Curve.Add(sdGx, sdGy, edPdx, new(big.Int).Mod(minusedPdy, Curve.P))
 
-	ebCheck := Sha256(Kdx.Bytes(), Kdy.Bytes(), m, BrianNodeNumber)
+	ebCheck := btcutils.Sha256(Kdx.Bytes(), Kdy.Bytes(), m, BrianNodeNumber)
 	ebCheckInt := new(big.Int).SetBytes(ebCheck)
 
 	if ebCheckInt.Cmp(eb) != 0 {
@@ -250,7 +212,7 @@ func Verify21AOSSig() {
 }
 
 func SubtractOnCurve(e []byte, Px, Py *big.Int) ([]byte, []byte, *big.Int) {
-	s, err := NewPrivateKey()
+	s, err := btcutils.NewPrivateKey()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -289,39 +251,39 @@ func testBorroeman() {
 	for i := 0; i < 3; i++ {
 		P[0][i] = make([]*big.Int, 2)
 
-		key, err := NewPrivateKey()
+		key, err := btcutils.NewPrivateKey()
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		x[i] = key
 
-		P[0][i][0], P[0][i][1] = PubkeyPointsFromPrivkey(key)
+		P[0][i][0], P[0][i][1] = btcutils.PubkeyPointsFromPrivkey(key)
 	}
 
 	for i := 0; i < 3; i++ {
 		P[1][i] = make([]*big.Int, 2)
 
-		key, err := NewPrivateKey()
+		key, err := btcutils.NewPrivateKey()
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		x[3+i] = key
 
-		P[1][i][0], P[1][i][1] = PubkeyPointsFromPrivkey(key)
+		P[1][i][0], P[1][i][1] = btcutils.PubkeyPointsFromPrivkey(key)
 	}
 
 	jistar := []int{1, 2, 3, 4, 5, 6} // indices of signer in each ring
 
-	M := Sha256([]byte("cool"))
+	M := btcutils.Sha256([]byte("cool"))
 
 	e := make(map[int]map[int][]byte)
 	s := make(map[int]map[int]*big.Int)
 	k := make(map[int]*big.Int)
 
 	for i := 0; i < 6; i++ {
-		ktemp, err := NewPrivateKey()
+		ktemp, err := btcutils.NewPrivateKey()
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -338,7 +300,7 @@ func testBorroeman() {
 		e[i] = make(map[int][]byte, len(loop))
 		s[i] = make(map[int]*big.Int, len(loop))
 
-		kiGx, kiGy := PubkeyPointsFromPrivkey(k[i])
+		kiGx, kiGy := btcutils.PubkeyPointsFromPrivkey(k[i])
 		kiG := append(kiGx.Bytes(), kiGy.Bytes()...)
 
 		jstari := jistar[i]
@@ -347,7 +309,7 @@ func testBorroeman() {
 			log.Fatal(err)
 		}
 
-		e[i][jstari+1] = Sha256(M, kiG, iByte, jstariByte)
+		e[i][jstari+1] = btcutils.Sha256(M, kiG, iByte, jstariByte)
 
 		mi := len(loop)
 		for j := jstari + 1; j < mi-1; j++ {
@@ -358,7 +320,7 @@ func testBorroeman() {
 
 			var tempx, tempy []byte
 			tempx, tempy, s[i][j] = SubtractOnCurve(e[i][j], P[i][j][0], P[i][j][1])
-			e[i][j+1] = Sha256(M, tempx, tempy, iByte, jByte)
+			e[i][j+1] = btcutils.Sha256(M, tempx, tempy, iByte, jByte)
 			log.Println(e[i][j+1])
 		}
 	}
@@ -372,7 +334,7 @@ func testBorroeman() {
 		toBeHashed = append(toBeHashed, temp...)
 	}
 
-	e0 := Sha256(toBeHashed)
+	e0 := btcutils.Sha256(toBeHashed)
 
 	for i := 0; i <= 1; i++ {
 		iByte, err := utils.ToByte(i)
@@ -391,7 +353,7 @@ func testBorroeman() {
 
 			tempx, tempy, s[i][j] = SubtractOnCurve(e[i][j], P[i][j][0], P[i][j][1])
 
-			e[i][j+1] = Sha256(M, tempx, tempy, iByte, jByte)
+			e[i][j+1] = btcutils.Sha256(M, tempx, tempy, iByte, jByte)
 
 			eijstari := new(big.Int).SetBytes(e[i][jistar[i]])
 
@@ -426,12 +388,12 @@ func testBorroeman() {
 			tempx, tempy := SubtractOnCurveS(ex[i][j], P[i][j][0], P[i][j][1], s[i][j])
 
 			r[i][j+1] = append(tempx, tempy...)
-			e[i][j+1] = Sha256(M, tempx, tempy, iByte, jplusoneByte)
+			e[i][j+1] = btcutils.Sha256(M, tempx, tempy, iByte, jplusoneByte)
 			log.Println(e[i][j+1])
 		}
 	}
 
-	e0prime := Sha256(r[0][2], r[1][2], M)
+	e0prime := btcutils.Sha256(r[0][2], r[1][2], M)
 	log.Println(e0prime, e0)
 }
 
@@ -458,75 +420,75 @@ func testElgamal() {
 	// note that we're only implementing the commitment scheme here not the signature scheme. Signature scheme could be
 	// AOS or something similar
 
-	x, err := NewPrivateKey()
+	x, err := btcutils.NewPrivateKey()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	r, err := NewPrivateKey()
+	r, err := btcutils.NewPrivateKey()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	xG := PointFromPrivkey(x)
+	xG := btcutils.PointFromPrivkey(x)
 
-	shaBytes := Sha256(Curve.Params().Gx.Bytes(), Curve.Params().Gy.Bytes()) // SHA256(G)
+	shaBytes := btcutils.Sha256(Curve.Params().Gx.Bytes(), Curve.Params().Gy.Bytes()) // btcutils.Sha256(G)
 
-	var H points.Point
-	H.Set(Curve.ScalarBaseMult(shaBytes)) // H = points.Point(SHA256(G))
+	var H btcutils.Point
+	H.Set(Curve.ScalarBaseMult(shaBytes)) // H = btcutils.Point(btcutils.Sha256(G))
 
-	var rH points.Point
+	var rH btcutils.Point
 	rH.Set(Curve.ScalarMult(H.X, H.Y, r.Bytes()))
 
-	var pedersen points.Point
+	var pedersen btcutils.Point
 	pedersen.Add(xG, rH)
 
-	var rG points.Point
-	rG = PointFromPrivkey(r)
+	var rG btcutils.Point
+	rG = btcutils.PointFromPrivkey(r)
 
-	var elgamal points.Elgamal
+	var elgamal btcutils.Elgamal
 	elgamal.Set(pedersen, rG)
 
 	log.Println("elgamal: ", elgamal)
 }
 
 func main() {
-	x, err := NewPrivateKey()
+	x, err := btcutils.NewPrivateKey()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	r, err := NewPrivateKey()
+	r, err := btcutils.NewPrivateKey()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	xG := PointFromPrivkey(x)
+	xG := btcutils.PointFromPrivkey(x)
 
-	shaBytes := Sha256(Curve.Params().Gx.Bytes(), Curve.Params().Gy.Bytes()) // SHA256(G)
+	shaBytes := btcutils.Sha256(Curve.Params().Gx.Bytes(), Curve.Params().Gy.Bytes()) // btcutils.Sha256(G)
 
-	var H points.Point
-	H.Set(Curve.ScalarBaseMult(shaBytes)) // H = points.Point(SHA256(G))
+	var H btcutils.Point
+	H.Set(Curve.ScalarBaseMult(shaBytes)) // H = btcutils.Point(btcutils.Sha256(G))
 
-	var rH points.Point
+	var rH btcutils.Point
 	rH.Set(Curve.ScalarMult(H.X, H.Y, r.Bytes()))
 
-	var pedersen points.Point
+	var pedersen btcutils.Point
 	pedersen.Add(xG, rH)
 
-	var rG points.Point
-	rG = PointFromPrivkey(r)
+	var rG btcutils.Point
+	rG = btcutils.PointFromPrivkey(r)
 
 	// xG+(r+H(xG+rH||rG))H is the switch commitment
 	// let the ugly term be p ie the commitment is xG = pH
-	insideHash := Sha256(pedersen.Bytes(), rG.Bytes())
+	insideHash := btcutils.Sha256(pedersen.Bytes(), rG.Bytes())
 	insideHashNumber := new(big.Int).SetBytes(insideHash)
 	p := new(big.Int).Add(r, insideHashNumber)
 
-	var pH points.Point
+	var pH btcutils.Point
 	pH.Set(Curve.ScalarMult(H.X, H.Y, p.Bytes()))
 
-	var switchCmt points.Point
+	var switchCmt btcutils.Point
 	switchCmt.Add(xG, pH)
 
 	log.Println("switch commitment: ", switchCmt)
